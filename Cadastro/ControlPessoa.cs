ac6a47aef1e784e943b22602062d4fcc5176bc7d
DIEGO DINIZ FLIEBER FERREIRA <diego.dfferreira1@senacsp.edu.br>
2024-01-23 22:26:04 -0300

    Primeiro Commit

---
+++ Cadastro/ControlPessoa.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro
+{
+    class ControlPessoa
+    {
+        private int opcao = 0;
+        DAO conectar;
+        public ControlPessoa() 
+        {
+            ConsultarOpcao = 0;  
+            conectar = new DAO(); //Conectando com o banco de dados
+        }//Fim do construtor
+
+        public int ConsultarOpcao
+        {
+            get { return this.opcao; }
+            set { this.opcao = value;  }
+        }//fim do getSet
+
+        public void Menu()
+        {
+            Console.WriteLine("Escolha uma das opções abaixo: \n" +
+                              "1. Cadastrar\n" +
+                              "2. Consultar\n" +
+                              "3. Atualizar\n" +
+                              "4. Excluir\n" +
+                              "5. Sair");
+            ConsultarOpcao = Convert.ToInt32(Console.ReadLine());
+        }//Fim do Menu
+
+        public void operacao()
+        {
+            do
+            {
+                Menu();//Mostrar as opções para o usuário
+                switch (ConsultarOpcao)
+                {
+                    case 1:
+                        Cadastrar();
+                        break;
+                    case 2:
+                        //Consultar
+                        break;
+                    case 3:
+                        //Atualizar
+                        break;
+                    case 4:
+                        //Excluir
+                        break;
+                    case 5:
+                        //Agradecer
+                        Console.WriteLine("Obrigado!");
+                        break;
+                    default:
+                        Console.WriteLine("Informe um código de acordo com o menu");
+                        break;
+                }//Fim do escolha caso
+            } while (ConsultarOpcao != 5);
+        }//fim do método
+
+        public void Cadastrar()
+        {
+            Console.WriteLine("Informe o nome da pessoa: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Informe o telefone da pessoa: ");
+            string telefone = Console.ReadLine();
+            Console.WriteLine("Informe o nome da cidade da pessoa: ");
+            string cidade = Console.ReadLine();
+            Console.WriteLine("Informe o endereço da pessoa: ");
+            string endereco = Console.ReadLine();
+            //Dados que vou inserir no BD
+            conectar.inserir(nome, telefone, cidade, endereco);
+
+        }//Fim do método cadastrar
+
+    }//Fim da classe
+}//Fim do Projeto
---
+++ Cadastro/DAO.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+
+namespace Cadastro
+{
+    class DAO
+    {
+        public MySqlConnection conexao;//COnectar ao banco de dados 
+        public string dados;
+        public string sql;
+        public string resultado;
+        public DAO()
+        {
+
+            conexao = new MySqlConnection("server=localhost;DataBase=ti18nPessoa;Uid=root;Password=");
+            try
+            {
+                conexao.Open();//Abri a conexão com o DB
+                Console.WriteLine("Conectado com sucesso!");
+            }
+            catch (Exception erro) 
+            {
+                Console.WriteLine("Algo deu errado! Verifique a conexão!\n\n" + erro);
+                conexao.Close();//Fechar a conexão com o DB
+            }//FIm do try catch
+        }//Fim do método
+
+        //Método inserir
+
+        public void inserir(string nome, string telefone, string cidade, string endereco)
+        {
+            try
+            {
+            
+                dados = "('','" + nome + "','" + telefone + "','" + cidade + "','" + endereco + "')";
+                sql = "insert into Pessoa(codigo, nome, telefone, cidade, endereco) values" + dados;
+
+                MySqlCommand conn = new MySqlCommand(sql, conexao);//Prepara a execução no banco de dados
+                resultado = "" + conn.ExecuteNonQuery();//Ctrl + Enter -> executando o comando no banco de dados
+                Console.WriteLine(resultado + "Linha afetada");
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine("Erro!!! Algo deu errado\n\n\n" + erro);
+            }//Fim do try catch
+        }//Fim do método
+
+
+    }//Fim da classe
+}//Fim do projeto
